e2f3e7f4ac459f0eede3f7a0e9e3a4ea1d9cca39
Rafael C <rafael.castro@upc.edu.pe>
2025-05-09 23:43:23 -0500

    First local commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {LayoutComponent} from './core/components/layout/layout.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [LayoutComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'learning-center';
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+import {HomeComponent} from './core/components/home/home.component';
+import {AboutComponent} from './core/components/about/about.component';
+import {CoursesComponent} from './domains/products/components/courses/courses.component';
+
+
+
+export const routes: Routes = [
+  { path: 'home-component', component: HomeComponent },
+  { path: 'about-component', component: AboutComponent },
+  { path: 'courses-component', component: CoursesComponent },
+];
